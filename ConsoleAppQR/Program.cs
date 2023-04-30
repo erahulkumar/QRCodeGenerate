e466c93ac71742feac1ed2d6f6fa3f1ca19e940a
Rahul-Kumar <91170678+erahulkumar@users.noreply.github.com>
2023-04-30 22:15:16 +0530

    QR

---
+++ ConsoleAppQR/Program.cs
@@ -0,0 +1,39 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+namespace ConsoleAppQR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Text to encode in QR code
+            //string text = "https://engineerboyz.in/";
+            string text;
+            Console.WriteLine("Enter qr code name ");
+            text = Console.ReadLine();
+            // Create QR code generator object
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+            // Generate QR code
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+            // Create QR code object
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            // Convert QR code to bitmap image
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Save QR code image to file
+            string txtimg;
+            Console.WriteLine("Enter qr code image name include (.png) ");
+            txtimg = Console.ReadLine();
+            qrCodeImage.Save(txtimg, ImageFormat.Png);
+            //qrCodeImage.Save("EngineerBoyz", ImageFormat.Png);
+            // Display success message
+            Console.WriteLine("QR code generated successfully.");
+            Console.ReadKey();
+        }
+    }
+}
